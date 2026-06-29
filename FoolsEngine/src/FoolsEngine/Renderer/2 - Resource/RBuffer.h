9d7730e36ebe4197c4c2047dc150b9ab3a2abb85
unknown <pawel.krzysztof.kaleta@gmail.com>
2026-06-29 08:13:01 +0200

    Simple GPU memory allocators

--- FoolsEngine/src/FoolsEngine/Renderer/2 - Resource/RBuffer.h
+++ FoolsEngine/src/FoolsEngine/Renderer/2 - Resource/RBuffer.h
@@ -35,17 +35,101 @@ namespace fe::Resource
 	class RAllocator
 	{
 	public:
-		virtual RMemReg AllocateRaw(UInt bytes) = 0;
-		virtual RMemReg AllocateRaw(UInt bytes, UInt alignment) = 0;
-		virtual void DeallocateRaw(RMemReg memReg) = 0;
+		virtual RMemReg Allocate(UInt bytes) = 0;
+		virtual RMemReg Allocate(UInt bytes, UInt alignment) = 0;
+		virtual void Deallocate(RMemReg memReg) = 0;
 
 		template <UInt Size, UInt Alignment>
-		RMemReg AllocateRaw() { return AllocateRaw(Size, Alignment); }
+		RMemReg Allocate() { return Allocate(Size, Alignment); }
 
 		template <UInt Alignment>
-		RMemReg AllocateRaw(UInt bytes) { return AllocateRaw(bytes, Alignment); }
+		RMemReg Allocate(UInt bytes) { return Allocate(bytes, Alignment); }
 
 		template <UInt Size>
-		void DeallocateRaw(RMemReg memReg) { DeallocateRaw(memReg); } // assert Size == memReg.Size
+		void Deallocate(RMemReg memReg) { Deallocate(memReg); } // assert Size == memReg.Size
 	};
+
+	class RArenaAlloc : public RAllocator
+	{
+	public:
+		RBuffer* Buffer;
+		UInt Free;
+
+		void Clear() { Free = 0; }
+
+		virtual RMemReg Allocate(UInt bytes) override final
+		{
+			RMemReg memReg;
+			memReg.Offset = (U32)AlignTo<8>((Byte*)Free);
+			UInt new_free = memReg.Offset + bytes;
+			if (new_free > Buffer->Size)
+			{
+				FE_CORE_ASSERT(false, "RArenaAllocator overflow!");
+				memReg.Buffer = nullptr;
+				return memReg;
+			}
+			memReg.Buffer = Buffer;
+			memReg.Size = bytes;
+			Free = new_free;
+			return memReg;
+		}
+		virtual RMemReg Allocate(UInt bytes, UInt alignment) override final
+		{
+			RMemReg memReg;
+			memReg.Offset = (U32)AlignTo((Byte*)Free, alignment);
+			UInt new_free = memReg.Offset + bytes;
+			if (new_free > Buffer->Size)
+			{
+				FE_CORE_ASSERT(false, "RArenaAllocator overflow!");
+				memReg.Buffer = nullptr;
+				return memReg;
+			}
+			memReg.Buffer = Buffer;
+			memReg.Size = bytes;
+			Free = new_free;
+			return memReg;
+		}
+		virtual void Deallocate(RMemReg memReg) override final { FE_CORE_ASSERT(false, "RArenaAllocator does not deallocate individually"); }
+
+		template <UInt Size, UInt Alignment>
+		RMemReg Allocate()
+		{
+			RMemReg memReg;
+			memReg.Offset = (U32)AlignTo<Alignment>((Byte*)Free);
+			UInt new_free = memReg.Offset + Size;
+			if (new_free > Buffer->Size)
+			{
+				FE_CORE_ASSERT(false, "RArenaAllocator overflow!");
+				memReg.Buffer = nullptr;
+				return memReg;
+			}
+			memReg.Buffer = Buffer;
+			memReg.Size = Size;
+			Free = new_free;
+			return memReg;
+		}
+
+		template <UInt Alignment>
+		RMemReg Allocate(UInt bytes)
+		{
+			RMemReg memReg;
+			memReg.Offset = (U32)AlignTo<Alignment>((Byte*)Free);
+			UInt new_free = memReg.Offset + bytes;
+			if (new_free > Buffer->Size)
+			{
+				FE_CORE_ASSERT(false, "RArenaAllocator overflow!");
+				memReg.Buffer = nullptr;
+				return memReg;
+			}
+			memReg.Buffer = Buffer;
+			memReg.Size = bytes;
+			Free = new_free;
+			return memReg;
+		}
+
+		template <UInt Size>
+		void Deallocate(Byte* ptr) { FE_CORE_ASSERT(false, "RArenaAllocator does not deallocate individually"); }
+	};
+
+
 }
