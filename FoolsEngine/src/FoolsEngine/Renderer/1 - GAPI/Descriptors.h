edaa5c6d915cffd20737887c068eee6b47254f3e
Pawel Kaleta <pawel.krzysztof.kaleta@gmail.com>
2026-09-15 18:30:40 +0200

    Work on C++ reprezentation of std140 and std 430 layout rules

--- FoolsEngine/src/FoolsEngine/Renderer/1 - GAPI/Descriptors.h
+++ FoolsEngine/src/FoolsEngine/Renderer/1 - GAPI/Descriptors.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "FoolsEngine/Foundation/Utils/DeclareEnum.h"
+#include "FoolsEngine/Foundation/Memory/Splice.h"
 
 #include <glm/glm.hpp>
 
@@ -93,5 +94,59 @@ namespace fe::GAPI::Descriptors
 		{
 			BlendFunction	mBlendFunction = BlendFunction::None;
 		};
+	}
+
+	namespace Data
+	{
+		struct test
+		{
+			bool x, y;
+		};
+
+		using std140_bool = U32;
+		struct alignas( 8) std140_bvec2 : fe::Array< std140_bool, 2 > {};
+		struct alignas(16) std140_bvec3 : fe::Array< std140_bool, 3 > {};
+		struct alignas(16) std140_bvec4 : fe::Array< std140_bool, 4 > {};
+
+		using std140_int = S32;
+		struct alignas( 8) std140_ivec2 : fe::Array< std140_int, 2 > {};
+		struct alignas(16) std140_ivec3 : fe::Array< std140_int, 3 > {};
+		struct alignas(16) std140_ivec4 : fe::Array< std140_int, 4 > {};
+
+		using std140_uint = U32;
+		struct alignas( 8) std140_uvec2 : fe::Array< std140_uint, 2 > {};
+		struct alignas(16) std140_uvec3 : fe::Array< std140_uint, 3 > {};
+		struct alignas(16) std140_uvec4 : fe::Array< std140_uint, 4 > {};
+
+		using std140_float = F32;
+		struct alignas( 8) std140_vec2 : fe::Array< std140_float, 2 > {};
+		struct alignas(16) std140_vec3 : fe::Array< std140_float, 3 > {};
+		struct alignas(16) std140_vec4 : fe::Array< std140_float, 4 > {};
+
+		struct std140_mat2x2 { std140_vec2 data[2]; };
+		struct std140_mat2x3 { std140_vec3 data[2]; };
+		struct std140_mat2x4 { std140_vec4 data[2]; };
+		struct std140_mat3x2 { std140_vec2 data[3]; };
+		struct std140_mat3x3 { std140_vec3 data[3]; };
+		struct std140_mat3x4 { std140_vec4 data[3]; };
+		struct std140_mat4x2 { std140_vec2 data[4]; };
+		struct std140_mat4x3 { std140_vec3 data[4]; };
+		struct std140_mat4x4 { std140_vec4 data[4]; };
+
+		template <typename T>
+		struct std140_Array
+		{
+			struct __declspec(align(16)) ElementType : T { };
+
+
+		};
+
+		using std140_double = F64;
+		struct alignas(16) std140_vec2 : fe::Array< std140_double, 2 > {};
+		struct alignas(32) std140_vec3 : fe::Array< std140_double, 3 > {};
+		struct alignas(32) std140_vec4 : fe::Array< std140_double, 4 > {};
+		
+
+
 	}
 }
